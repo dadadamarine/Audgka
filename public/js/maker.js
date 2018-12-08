f758e9a3cef59f1e4454769018c99f9012462519
dadadamarine <dadadamarine@naver.com>
2018-12-08 19:00:36 +0900

    #maker 텍스트에디터 뜨는거 중앙으로 보정

--- public/js/maker.js
+++ public/js/maker.js
@@ -56,12 +56,43 @@ window.onload=function(){
 }
 
 function openToolBox(e){
-    document.getElementById("toolBox").style="display:block;"
+    document.getElementById("toolBox").style="display:block;";
 }
 function closeToolBox(e){
     document.getElementById("toolBox").style="display:none;"
 }
 function openTextEditBox(e){
+    //window 기준 클릭한 요소의 offset
+    let elementOffset= e.target.getBoundingClientRect(); 
+    let elementTop = elementOffset.top;
+    let elementLeft= elementOffset.left;
+    //window에서 명함 상자까지의 offset
+    let cardOffset = document.getElementById("P1-wrap").getBoundingClientRect();
+    let cardTop = cardOffset.top;
+    let cardLeft = cardOffset.left;
+
+/*     let cardBlockOffset = document.getElementById("PGs-Wrap").getBoundingClientRect();
+    let cardBlockLeft = cardBlockOffset.left;
+    // 카드~ 요소까지의 거리 */
+
+    console.log("offset들 엘리먼트, 카드순",elementTop, elementLeft, cardTop, cardLeft);
+    let top = elementTop - cardTop - 45; // 30은 text 툴바의 높이.
+    let left = elementLeft - cardLeft ; //+ cardBlockLeft
+
+    // 텍스트상자는 요소의 넓이, 높이의 1.5배 가져감.
+    let width = e.target.offsetWidth * 1.5;
+    let height = e.target.offsetHeight * 1.5;
+    let qu= document.getElementById("textArea").style="width:"+ width+"px ; height:" + height +"px ;";
+    console.log(qu);
+
+    //위치 반영
+    let styleQuery= "position:absolute; top:"+top+"px; left:"+left+"px;  transform:translate(-16.66%, -16.66%);";
+    //추가된 넓이 1/3의 절반인 1/6만큼 왼쪽으로 이동. , 위쪽으로는 1/6 + 툴바의 높이만큼
+    //let styleQuery= "position:absolute; top:"+top+"px; left:"+left+"px;";
+    console.log(styleQuery);
+    document.getElementById("textEditBox").style=styleQuery;
+
+    //텍스트 상자 열기
     document.getElementById("textEditBoxCover").style="display:block;"
 }
 function closeTextEditBox(e){
